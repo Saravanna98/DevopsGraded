9f6c2ae24a4d9b62fb88a66372e53ec9289d3c0d
Saravanna98 <85392033+Saravanna98@users.noreply.github.com>
2021-06-07 13:16:21 +0530

    Update DevopsGradedAssignmentApplication.java

--- src/main/java/com/hcl/devops/DevopsGradedAssignmentApplication.java
+++ src/main/java/com/hcl/devops/DevopsGradedAssignmentApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevopsGradedAssignmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevopsGradedAssignmentApplication.class, args);
+		//SpringApplication.run(DevopsGradedAssignmentApplication.class, args);
 		System.out.println("Hello Dev-Ops");
 	}
 
